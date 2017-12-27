bc11826dcd53b6bc72479b18e5a5dd27350839c3
Alfonso <alfonsowillhit@libero.it>
2017-12-27 12:32:30 +0100

    Creata classe connessione db

---
+++ src/utils/DBConnection.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.sql.*;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class DBConnection {
+
+	public static Connection getConnection() throws SQLException {
+		
+
+		Connection connection = null;
+		System.out.println("-------- MySQL JDBC Test Connessione ------------");
+
+		  try {
+		   Class.forName("com.mysql.jdbc.Driver");
+		  } catch (ClassNotFoundException e) {
+		   System.out.println("Where is your MySQL JDBC Driver?");
+		   e.printStackTrace();
+		   return connection;
+		  }
+
+		  System.out.println("MySQL JDBC Driver registrato!");
+		  
+
+		  try {
+		   connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/progettoweb","root", "root");
+
+		  } catch (SQLException e) {
+			  JOptionPane.showMessageDialog(new JFrame(), "Connessione al DataBase fallita!" + e.getMessage());
+		   return connection;
+		  }
+
+		  if (connection != null) {
+		   System.out.println("Connesso al DB!");
+		  } else {
+		   System.out.println("Failed to make connection!");
+		  }
+		return connection;
+		  
+		  
+		  
+	}
+
+}
