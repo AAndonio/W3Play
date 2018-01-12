07b33b286ff48e8f062561c4d10fae3e6982ef47
Antonio <ndonio96@gmail.com>
2018-01-12 14:13:25 +0100

    Aggiunta pagina di stile per pagina admin

---
+++ src/utils/Schema.java
@@ -0,0 +1,28 @@
+package utils;
+
+/**
+ * 
+ * @author Luca
+ *
+ */
+public class Schema {
+	
+	public class Amministratore {
+		
+		public static final String EMAIL = "Email";
+		public static final String RUOLO = "Ruolo";
+	}
+	
+	public class Utente {
+		
+		public static final String VIA = "Via";
+		public static final String CAP = "Cap";
+		public static final String CITTA = "Citta";
+		public static final String NOME = "Nome";
+		public static final String EMAIL = "Email";
+		public static final String CIVICO = "Civico";
+		public static final String COGNOME = "Cognome";
+		public static final String PASSWORD = "Password";
+		
+	}
+}
