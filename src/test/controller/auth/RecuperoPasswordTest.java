9e530a159949cd1e5141072a1c7bb28c262ca350
Luca <anzluca96@gmail.com>
2018-01-18 12:23:11 +0100

    eliminazione StatisticheTest e rinominazione di EmailControlServlet in RecuperoPassword

---
+++ src/test/controller/auth/RecuperoPasswordTest.java
@@ -0,0 +1,16 @@
+package test.controller.auth;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecuperoPasswordTest {
+
+	@Test
+	public void test() {
+		
+		String email = "mail123.prova@dominio.com";
+		
+	}
+
+}
